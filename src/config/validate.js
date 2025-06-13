9a52464153723753cf563010c74c58404c55f6da
Chuks <63956312+skido600@users.noreply.github.com>
2025-06-13 17:18:15 +0200

    update

--- src/config/validate.js
+++ src/config/validate.js
@@ -5,11 +5,22 @@ const Loginschema = joi.object({
     .min(6)
     .max(60)
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
+    .required()
+    .messages({
+      "string.email": "Email must be a valid address with .com or .net",
+      "string.min": "Email must be at least 6 characters",
+      "string.max": "Email must be less than or equal to 60 characters",
+      "string.empty": "Email is required",
+    }),
   password: joi
     .string()
     .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$"))
-    .required(),
+    .required()
+    .messages({
+      "string.pattern.base":
+        "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one number.",
+      "string.empty": "Password is required",
+    }),
 });
 const postSchema = joi.object({
   title: joi.string().min(5).max(200).required(),
--- src/routes/loginroute.js
+++ src/routes/loginroute.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { login, logout } from "../controller/authcontrollers.js";
-// import authenticate from "../middlewares/verify.js";
+import authenticate from "../middlewares/verify.js";
 const router = express.Router();
 
 // POST /api/login
 router.post("/login", login);
-router.post("/logout", logout);
-// router.post("/logout", authenticate, Logout);
+router.post("/logout", authenticate, logout);
+
 export default router;
